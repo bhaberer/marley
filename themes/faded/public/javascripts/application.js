0df2812efe7cd07573db7c9861ede2aee3925d34
Brian Haberer <bhaberer@gmail.com>
2010-09-03 01:29:30 -0700

    Started some css work, moved to sass, fixed erb skeleton that paulv spotted.

--- themes/faded/public/javascripts/application.js
+++ themes/faded/public/javascripts/application.js
@@ -1,1 +1,13 @@
 // application.js
+
+$(document).ready(function($) {
+  twttr.anywhere(function(twitter) {
+    twitter.hovercards({ expanded: true });
+  });
+});
+
+$(document).ready(function($) {
+  twttr.anywhere(function (T) {
+    T('#followme').followButton("weirdo513");
+  });
+});
